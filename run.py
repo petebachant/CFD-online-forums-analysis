f03a0e904198e1b2f735eaa9b5bc001e3d917d12
Pete Bachant <petebachant@gmail.com>
2015-09-27 13:46:29 -0400

    Remake run.py from notebook

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
-#!/usr/bin/env python
-"""
-This script parses the cfd-online.com main forum page for thread and post
-counts.
-"""
 
+# coding: utf-8
+
+# # CFD-Online community analysis
+# 
+# In this notebook we will examine the most popular CFD software forums on CFD-online.com. 
+
+# In[ ]:
+
+import matplotlib.pyplot as plt
+get_ipython().magic('matplotlib inline')
+import seaborn as sns
+from pxl.styleplot import set_sns
+set_sns()
+plt.rcParams["axes.grid"] = True
+plt.rcParams["axes.formatter.limits"] = (-4, 4)
 import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
+import os
 
 soup = BeautifulSoup(requests.get("http://cfd-online.com/Forums/").text, "lxml")
 
@@ -29,7 +40,7 @@ for n in numbers:
         posts[forum] = int(n.text.replace(",", ""))
 
 # Create a DataFrame from the dictionaries
-df = pd.DataFrame()
+df = pd.DataFrame(columns=["forum", "threads", "posts"])
 
 for forum in forums:
     s = pd.Series()
@@ -38,4 +49,31 @@ for forum in forums:
     s["posts"] = posts[forum]
     df = df.append(s, ignore_index=True)
 
-print(df)
+df = df.set_index("forum")
+df["pt_ratio"] = df.posts/df.threads
+
+
+# In[ ]:
+
+# Create a slice of that DataFrame for only the most popular forums
+df2 = df[df.posts > 1e4].sort("posts")
+df2 = df2.drop("posts", 1)
+
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(7.5, 3), sharey=True)
+
+df2.threads.plot(ax=ax1, kind="barh")
+df2.pt_ratio.plot(ax=ax2, kind="barh", color=sns.color_palette()[2])
+
+ax1.set_ylabel("")
+ax2.set_ylabel("")
+ax1.set_xlabel("Threads")
+ax2.set_xlabel("Posts/threads")
+fig.tight_layout()
+
+if not os.path.isdir("figures"):
+    os.mkdir("figures")
+
+fig.savefig("figures/cfd-online.pdf")
+
+plt.show()
+
